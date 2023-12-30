8091433ab257abfd513e52ef51370c1d8299eee5
Wulabalabo <cyberchenjw@gmail.com>
2023-12-31 00:39:20 +0800

    initial project

---
+++ tailwind_niky/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <section className='xl:padding-1 wide:padding-r padding-b'>Hero</section>
+    </main>
+  )
+}
